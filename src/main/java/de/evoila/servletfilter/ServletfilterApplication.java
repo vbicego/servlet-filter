f0995935ff80adef8ab5958609b4bbcce1b9cda8
Victor Bicego <vbicego@evoila.de>
2022-01-08 13:41:05 +0100

    First commit

---
+++ src/main/java/de/evoila/servletfilter/ServletfilterApplication.java
@@ -0,0 +1,13 @@
+package de.evoila.servletfilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServletfilterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServletfilterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/evoila/servletfilter/ServletfilterApplicationTests.java
@@ -0,0 +1,13 @@
+package de.evoila.servletfilter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServletfilterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
