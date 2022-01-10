3a4b51abaee2ab709fec2c55763e6896a2b47a12
Victor Bicego <vbicego@evoila.de>
2022-01-10 09:25:36 +0100

    Add a filter who allows only even requests

--- src/main/java/de/evoila/servletfilter/SimpleFilter.java
+++ src/main/java/de/evoila/servletfilter/SimpleFilter.java
@@ -4,9 +4,12 @@ import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Component
 public class SimpleFilter implements Filter {
+
+    private final AtomicLong counter = new AtomicLong();
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -16,9 +19,14 @@ public class SimpleFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
 
-        System.out.println("Remote Host:" + servletRequest.getRemoteHost());
-        System.out.println("Remote Address:" + servletRequest.getRemoteAddr());
-        filterChain.doFilter(servletRequest, servletResponse);
+//        System.out.println("Remote Host:" + servletRequest.getRemoteHost());
+//        System.out.println("Remote Address:" + servletRequest.getRemoteAddr());
+
+        if (counter.incrementAndGet() % 2 == 0) {
+            filterChain.doFilter(servletRequest, servletResponse);
+        } else {
+            System.out.println("Access denied, try again");
+        }
 
     }
 
